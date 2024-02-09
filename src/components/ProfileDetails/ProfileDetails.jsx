064fbf3a973c4ed3837695e6d131f182c3ed69d9
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2024-02-09 15:28:59 +0530

    Changed tab name.

--- src/components/ProfileDetails/ProfileDetails.jsx
+++ src/components/ProfileDetails/ProfileDetails.jsx
@@ -16,6 +16,7 @@ const ProfileDetails = () => {
         </h3>
         <p className='my-3 text-lg text-gray-300 '>My introduction</p><br/>
         <div className='flex flex-col-reverse items-center max-w-6xl gap-12 px-10 mx-auto md:flex-row md:gap-6'>
+          
           <div className='px-10'>
             <div className='my-3 text-zinc-400'>
               <p className='px-8 mx-auto text-xl leading-7 text-justify '>As a passionate and talented software engineering student, I am eager to learn and adapt to new challenges. Collaboration is my forte, and I thrive in team environments, valuing diverse perspectives. My academic journey has equipped me with a strong foundation in software engineering principles, which I continually apply to real-world scenarios through internships and personal projects. I aspire to make positive contributions to both the industry and society. 
@@ -37,16 +38,17 @@ const ProfileDetails = () => {
               </div>   
             </div>
           </div>
+
           <div className='flex items-center justify-center flex-1 mt-6 md:mt-0'>
             <div className='relative w-11/12 h-full max-w-sm md:w-60 lg:w-96 sm:w-10/12 -z-10 aboutImg'>
               <img src={aboutImg} alt='' className='object-cover w-full bg-cyan-600 rounded-3xl'/>
             </div>
           </div>
+
         </div>
       </div>
     </section>
   );
 };
-
 export default ProfileDetails;
 

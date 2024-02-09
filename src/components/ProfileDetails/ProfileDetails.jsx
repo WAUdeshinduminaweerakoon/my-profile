cf0f771dbae7e9f0624c6c03314006cfcb43a79c
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2024-02-09 12:50:33 +0530

    Towards modernization of the Profile Details  section and add my cv

--- src/components/ProfileDetails/ProfileDetails.jsx
+++ src/components/ProfileDetails/ProfileDetails.jsx
@@ -1,32 +1,29 @@
-
-
 import React from 'react';
 import aboutImg from '../../assets/images/my photo.jpg';
-
+import cvPDF from '../../assets/My CV/Udesh Indumina (3).pdf';
 
 const ProfileDetails = () => {
   const info =[
     {text:"Years experience", count:"01"},
-    {text:"Completed Projects", count:"01"},
-    {text:"Companies Work", count:"01"},
-  ]
+    {text:"Completed Projects", count:"05"},
+  ];
+
   return (
-    <section className='py-10 text-white '>
+    <section className='px-2 py-10 text-white '>
       <div className='mt-8 text-center'>
         <h3 className='text-4xl font-semibold'>
           About <span className=' text-neutral-300'>Me</span>
         </h3>
         <p className='my-3 text-lg text-gray-300 '>My introduction</p><br/>
         <div className='flex flex-col-reverse items-center max-w-6xl gap-12 px-10 mx-auto md:flex-row md:gap-6'>
-          <div className='p-2'>
+          <div className='px-10'>
             <div className='my-3 text-zinc-400'>
-              <p className='w-11/12 mx-auto leading-7 text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste et hic qui! Quas
-                 incidunt beatae itaque aperiam doloremque consequatur molestiae!
-                  Officia numquam earum molestiae eaque, magni atque mollitia ullam obcaecati.
+              <p className='px-8 mx-auto text-xl leading-7 text-justify '>As a passionate and talented software engineering student, I am eager to learn and adapt to new challenges. Collaboration is my forte, and I thrive in team environments, valuing diverse perspectives. My academic journey has equipped me with a strong foundation in software engineering principles, which I continually apply to real-world scenarios through internships and personal projects. I aspire to make positive contributions to both the industry and society. 
+              <br/>Thank you for visiting my portfolio.
               </p>
               <div className='flex items-center mt-10 gap-7'>
                 {info.map((content)=>(
-                  <div key={content.text}>
+                  <div  className='w-9/12' key={content.text}>
                     <h3 className='text-2xl font-semibold text-white md:text-4xl'>{content.count}<span>+</span>{" "}</h3>
                     <span className='text-xl text-slate-400 md:text-base'>{content.text}</span>
                   </div>
@@ -34,13 +31,13 @@ const ProfileDetails = () => {
               </div>
               <div>
                   <br/>
-                  <a href='/My CV/Udesh Indumina CV.pdf' download>
+                  <a href={cvPDF} download>
                     <button className='btn-primary'>Download My CV</button>
                   </a>             
               </div>   
             </div>
           </div>
-          <div className='flex justify-center flex-1 md:mt-0 mt-6items-center'>
+          <div className='flex items-center justify-center flex-1 mt-6 md:mt-0'>
             <div className='relative w-11/12 h-full max-w-sm md:w-60 lg:w-96 sm:w-10/12 -z-10 aboutImg'>
               <img src={aboutImg} alt='' className='object-cover w-full bg-cyan-600 rounded-3xl'/>
             </div>
@@ -52,3 +49,4 @@ const ProfileDetails = () => {
 };
 
 export default ProfileDetails;
+
